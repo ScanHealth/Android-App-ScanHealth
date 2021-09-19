cac86f656c5f9f651e4acaeaeee031d8373db6f1
Nours86 <claragmtt@gmail.com>
2021-09-19 18:55:17 +0200

    Test new remote

--- app/src/main/java/fr/kevin/ptutapp/HomeActivity.java
+++ app/src/main/java/fr/kevin/ptutapp/HomeActivity.java
@@ -1,9 +1,7 @@
 package fr.kevin.ptutapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +13,8 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Initialize bottom navBar
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
